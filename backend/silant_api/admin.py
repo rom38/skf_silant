c1342e1856164eca0513d0e7a95db65511a06572
Rman_stat <roman_imp@mail.ru>
2023-12-02 00:11:06 +0800

    add some models

--- backend/silant_api/admin.py
+++ backend/silant_api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import MachineModel, EngineModel, TransmissionModel
+from .models import DrivelineModel, SteeringAxelModel, MaintenanceType
+from .models import FailureComponent, RestorationMethod
+from .models import MaintenanceOrganization, Machine
 
 # Register your models here.
+
+
+admin.site.register(MachineModel)
+admin.site.register(EngineModel)
+admin.site.register(TransmissionModel)
+admin.site.register(SteeringAxelModel)
+admin.site.register(DrivelineModel)
+admin.site.register(MaintenanceType)
+admin.site.register(FailureComponent)
+admin.site.register(RestorationMethod)
+admin.site.register(MaintenanceOrganization)
+admin.site.register(Machine)
--- backend/silant_api/models.py
+++ backend/silant_api/models.py
@@ -8,11 +8,11 @@ class MachineModel(models.Model):
     description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     class Meta:
-        verbose_name = "Модель погрузчика"
-        verbose_name_plural = "Модели погрузчика"
+        verbose_name = "Модель техники"
+        verbose_name_plural = "Модели техники"
         ordering = ["name"]
 
 
@@ -21,11 +21,11 @@ class EngineModel(models.Model):
     description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     class Meta:
         verbose_name = "Модель двигателя"
-        verbose_name_plural = "Модели двигателя"
+        verbose_name_plural = "Модели двигателей"
         ordering = ["name"]
 
 
@@ -34,9 +34,112 @@ class TransmissionModel(models.Model):
     description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     class Meta:
         verbose_name = "Модель трансмиссии"
-        verbose_name_plural = "Модели трансмиссии"
+        verbose_name_plural = "Модели трансмиссий"
         ordering = ["name"]
+
+
+class DrivelineModel(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "Модель ведущего моста"
+        verbose_name_plural = "Модели ведущих мостов"
+        ordering = ["name"]
+
+
+class SteeringAxelModel(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "Модель управляемого моста"
+        verbose_name_plural = "Модели управляемых мостов"
+        ordering = ["name"]
+
+
+class MaintenanceType(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "Вид технического обслуживания"
+        verbose_name_plural = "Виды технического обслуживания"
+        ordering = ["name"]
+
+
+class FailureComponent(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "Узел отказа"
+        verbose_name_plural = "Узлы отказа"
+        ordering = ["name"]
+
+
+class RestorationMethod(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "Способ восстановления"
+        verbose_name_plural = "Способы восстановления"
+        ordering = ["name"]
+
+
+class MaintenanceOrganization(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "Сервисная компания"
+        verbose_name_plural = "Сервисные компании"
+        ordering = ["name"]
+
+
+class Machine(models.Model):
+    machine_model_id = models.ForeignKey(MachineModel, on_delete=models.PROTECT)
+    machine_serial = models.CharField(
+        max_length=20, verbose_name="Зав. № машины", unique=True
+    )
+    engine_model_id = models.ForeignKey(EngineModel, on_delete=models.PROTECT)
+    engine_serial = models.CharField(
+        max_length=20, verbose_name="Зав. № двигателя", unique=True
+    )
+    transmission_model_id = models.ForeignKey(
+        TransmissionModel, on_delete=models.PROTECT
+    )
+    transmission_serial = models.CharField(
+        max_length=20, verbose_name="Зав. № трансмиссии", unique=True
+    )
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "Машина"
+        verbose_name_plural = "Машины"
+        ordering = ["engine_serial"]
