dd0a348f99e0bf6fa5aaa2265e75d8ebdbf9c887
rom38-alpine <you@example.com>
2023-12-01 01:48:58 +0000

    upgrade

--- backend/silant_api/admin.py
+++ backend/silant_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MachineModel, EngineModel, TransmissionModel
 
+admin.site.register(MachineModel)
+admin.site.register(EngineModel)
+admin.site.register(TransmissionModel)
 # Register your models here.
