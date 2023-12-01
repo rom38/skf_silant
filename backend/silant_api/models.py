f92a2207f061ac869210257b967e26445d0ec68f
Rman_stat <roman_imp@mail.ru>
2023-12-01 22:07:51 +0800

    add three model

--- backend/silant_api/models.py
+++ backend/silant_api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MachineModel(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Модель погрузчика"
+        verbose_name_plural = "Модели погрузчика"
+        ordering = ["name"]
+
+
+class EngineModel(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Модель двигателя"
+        verbose_name_plural = "Модели двигателя"
+        ordering = ["name"]
+
+
+class TransmissionModel(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Модель трансмиссии"
+        verbose_name_plural = "Модели трансмиссии"
+        ordering = ["name"]
