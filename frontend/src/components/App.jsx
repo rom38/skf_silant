bae6ea2cc1a58ba7482440d1c052c53b3b106e13
rom38-alpine <you@example.com>
2023-12-08 09:15:59 +0000

    redirect

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,6 +9,7 @@ import SearchPage from "./SearchPage";
 import { Layout } from "./Layout";
 // import ResultsPage from "./ResultsPage";
 import ProtectedRoute from "./ProtectedRoute";
+import Redirect from "./Redirect";
 
 // import Categories from "./Categories";
 // import SwaggerUIWidget from "./SwaggerUIWidget";
@@ -25,6 +26,8 @@ function App() {
                     <Route path="main" element={<MainPage />} />
                     <Route path="login" element={<LoginPage />} />
                     <Route path="faq" element={<AboutPage />} />
+                    <Route path="admin" element={<Redirect url={'http://127.0.0.1:8000/admin'} />} />
+                    <Route path="api" element={<Redirect url={'http://127.0.0.1:8000/api'} />} />
 
                     {/* <Route path="search" element={<SearchPage />} /> */}
                     <Route path="search" element={
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,32 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    proxy: {
+        // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+        '/api': 'http://localhost:8000',
+        '/static': 'http://localhost:8000',
+        '/admin': 'http://localhost:8000',
+
+        // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+        // '/api': {
+        //   target: 'http://jsonplaceholder.typicode.com',
+        //   changeOrigin: true,
+        //   rewrite: (path) => path.replace(/^\/api/, ''),
+        // },
+        // // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
+        // '^/fallback/.*': {
+        //   target: 'http://jsonplaceholder.typicode.com',
+        //   changeOrigin: true,
+        //   rewrite: (path) => path.replace(/^\/fallback/, ''),
+        // },
+        // // Using the proxy instance
+        // '/api': {
+        //   target: 'http://jsonplaceholder.typicode.com',
+        //   changeOrigin: true,
+        //   configure: (proxy, options) => {
+        //     // proxy will be an instance of 'http-proxy'
+        //   },
+        //},
+    },
 })
