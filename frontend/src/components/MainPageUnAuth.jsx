90fd9a27ccdad269891856f1ec1961eec437907e
Rman_stat <roman_imp@mail.ru>
2023-12-15 18:27:41 +0800

    some form upgrade

--- frontend/src/components/MainPageUnAuth.jsx
+++ frontend/src/components/MainPageUnAuth.jsx
@@ -8,8 +8,8 @@ import { Box, Button, Flex, Text, Input, Center } from "@chakra-ui/react";
 import { FormLabel, FormControl, FormErrorMessage } from "@chakra-ui/react";
 import { useGetWhoAmIQuery } from "../services/apiScan";
 import { useGetIsAuthQuery } from "../services/apiScan";
-import { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { useForm, Controller, set } from "react-hook-form";
 import { useMachineMutation } from "../services/apiScan";
 
 
@@ -21,6 +21,8 @@ function MainPageUnAuth() {
     const { data: dataAuth, error: errorAuth, isLoading, isError: isErrorAuth } = useGetIsAuthQuery();
     const { data: whoAmIData, error: errorWhoAmI, isLoading: isLoadingWhoAmI, refetch: refetchWhoAmI } = useGetWhoAmIQuery({ skip: (errorAuth !== undefined) });
     const [page, setPage] = useState("main")
+    const [errorMachineNotFound, setErrorMachineNotFound] = useState(false);
+
     const {
         handleSubmit,
         register,
@@ -33,29 +35,35 @@ function MainPageUnAuth() {
             machineNumber: "0045",
         },
     });
+
+    useEffect(() => {
+        return setErrorMachineNotFound(false)
+    })
+
     const [fetchMachine, { isLoading: isLoadingMachine, error: errorMachine,
         isError: isErrorMachine }] = useMachineMutation();
-    // const onSubmit = async (data, e) => {
+    const onSubmit = async (data, e) => {
 
-    //     e.preventDefault()
-    //     try {
-    //         const credentials = await login(data).unwrap()
-    //         console.log('from rtk', credentials)
-    //         setIsAuthError(false);
-    //         refetchCSRF();
+        e.preventDefault()
+        try {
+            const machineData = await fetchMachine(data.machineNumber).unwrap()
+            console.log('from main UnAuth', machineData)
+            // setIsAuthError(false);
+            // refetchCSRF();
 
-    //         console.log('from rtk Auth', data)
-    //         // refetchAuth();
-    //         // navigate('/')
-    //     } catch (err) {
-    //         console.log('error fetch token', err)
-    //         console.log('error fetch login auth', isErrorAuth)
-    //         setIsAuthError(true);
-    //         refetchCSRF();
-    //     }
-    //     console.log('form submit', data);
-    //     reset();
-    // };
+            // console.log('from rtk Auth', data)
+            // refetchAuth();
+            // navigate('/')
+        } catch (err) {
+            console.log('error fetch token', errorMachine)
+            if (errorMachine?.status === 404) setErrorMachineNotFound(true)
+            // console.log('error fetch login auth', isErrorAuth)
+            // setIsAuthError(true);
+            // refetchCSRF();
+        }
+        console.log('form submit', data);
+        // reset();
+    };
 
 
     console.log("whoami from main", whoAmIData)
@@ -67,57 +75,47 @@ function MainPageUnAuth() {
             <Text color="silant-b.300" fontSize="2rem" fontWeight="bold" align="center" m="30px">
                 Проверьте комплектацию и технические характеристики техники Силант
             </Text>
-            <FormControl isInvalid={errors.machineNumber}>
-                <Flex alignItems="center" justifyContent="center" gap="0.5rem">
-                    <FormLabel color="silant-b.300" fontSize="1.5em"
-                        mb="0px" htmlFor="machineNumber">Заводской номер</FormLabel>
-                    <Controller
-                        control={control}
-                        name="machineNumber"
-                        rules={{
-                            required: "Это поле необходимо", minLength: {
-                                value: 4,
-                                message: "Минимальная длина номера должна быть 4"
-                            }
-                        }}
-
-                        render={({ field: { onChange, onBlur, value, ref } }) => (
-                            <Input
-                                width="7rem"
-                                borderColor="silant-b.700"
-
-                                id="machineNumber"
-                                placeholder="номер"
-                                onChange={onChange}
-                                onBlur={onBlur}
-                            />)}
-                    />
-                    {/* <Input width="7rem"
-                            borderColor="silant-b.700"
-
-                            id="machineNumber"
-                            placeholder="номер"
-                            {...register("machineNumber", {
-                                required: "Это поле необходимо",
-                                minLength: {
+            <form onSubmit={handleSubmit(onSubmit)} id="machine-form">
+                <FormControl isInvalid={errors.machineNumber || errorMachineNotFound} onSubmit={handleSubmit(onSubmit)}>
+                    <Flex alignItems="center" justifyContent="center" gap="0.5rem">
+                        <FormLabel color="silant-b.300" fontSize="1.5em"
+                            mb="0px" htmlFor="machineNumber">Заводской номер</FormLabel>
+                        <Controller
+                            control={control}
+                            name="machineNumber"
+                            rules={{
+                                required: "Это поле необходимо", minLength: {
                                     value: 4,
                                     message: "Минимальная длина номера должна быть 4"
                                 }
-                            })}
-                        /> */}
-                    < Button colorScheme="silant-b"
-                        isLoading={isSubmitting} type="submit"
-                        isDisabled={errors.machineNumber}>
-                        Поиск
-                    </Button>
-                </Flex>
-                <Center>
-                    <FormErrorMessage>
-                        {errors.machineNumber && errors.machineNumber.message}
-                    </FormErrorMessage>
-                </Center>
-            </FormControl>
+                            }}
 
+                            render={({ field: { onChange, onBlur, value, ref } }) => (
+                                <Input
+                                    type="number"
+                                    width="7rem"
+                                    borderColor="silant-b.700"
+
+                                    id="machineNumber"
+                                    placeholder="номер"
+                                    onChange={onChange}
+                                    onBlur={onBlur}
+                                />)}
+                        />
+                        < Button colorScheme="silant-b"
+                            isLoading={isSubmitting} type="submit"
+                            isDisabled={errors.machineNumber}>
+                            Поиск
+                        </Button>
+                    </Flex>
+                    <Center>
+                        <FormErrorMessage>
+                            {errors.machineNumber && errors.machineNumber.message}
+                            {errorMachineNotFound && "Машины с таким номером не найдено"}
+                        </FormErrorMessage>
+                    </Center>
+                </FormControl>
+            </form>
 
 
             <Text color="silant-b.300" fontSize="2rem" fontWeight="bold" align="center" m="30px">
