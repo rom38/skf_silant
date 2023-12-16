07ce44d13c5706f3a7c166889ef90084aa250347
Rman_stat <roman_imp@mail.ru>
2023-12-17 00:09:17 +0800

    style and filter upgrade

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import LoginForm2 from "./LoginForm2"
 import WrapTable from "./TableComp";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
-import { Input, Select, FormLabel } from "@chakra-ui/react";
+import { Input, Select, FormLabel, FormControl } from "@chakra-ui/react";
 import { Center, HStack } from "@chakra-ui/react";
 import { useGetWhoAmIQuery } from "../services/apiScan";
 import { useGetIsAuthQuery } from "../services/apiScan";
@@ -14,7 +14,7 @@ import { useState, useMemo } from "react";
 import { useId } from "react";
 
 import SwaggerUI from "swagger-ui-react";
-import { sortBy, reverse } from "lodash";
+import { sortBy, reverse, uniqBy, chain, filter } from "lodash";
 import "swagger-ui-react/swagger-ui.css";
 
 // import style from "../styles/MainPage.module.css";
@@ -25,27 +25,106 @@ function MainPage() {
     const { data: dataAuth, error: errorAuth, isLoading, isError: isErrorAuth } = useGetIsAuthQuery();
     const { data: whoAmIData, error: errorWhoAmI,
         isLoading: isLoadingWhoAmI, refetch: refetchWhoAmI } = useGetWhoAmIQuery({ skip: (errorAuth !== undefined) });
-    const { data: machinesData, error: errorMachines, isLoading: isLoadingMachines, refetch: refetchMachines } = useGetMachinesQuery();
+    const { data: machinesData = [], error: errorMachines, isLoading: isLoadingMachines, refetch: refetchMachines } = useGetMachinesQuery();
     const [page, setPage] = useState("main");
+
+    const [serial, setSerial] = useState("все");
+    const [engine, setEngine] = useState("все");
+    const [transmission, setTransmission] = useState("все");
+    const [driveline, setDriveline] = useState("все");
+    const [steeringAxel, setSteeringAxel] = useState("все");
 
     const sortedMachinesData = useMemo(() => {
         return sortBy(machinesData, "pk").reverse()
     }, [machinesData])
 
     const filteredMachinesData = useMemo(() => {
-        return sortBy(machinesData, "pk").reverse()
+        console.log("serial", serial)
+        console.log("engine", engine)
+
+        var machinesData_int = [...machinesData]
+
+        if (serial !== "все") machinesData_int = machinesData_int
+            .filter(item => item['machine_model_name'] === serial)
+
+        if (engine !== "все") machinesData_int = machinesData_int
+            .filter(item => item['engine_model_name'] === engine)
+
+        if (transmission !== "все") machinesData_int = machinesData_int
+            .filter(item => item['transmission_model_name'] === transmission)
+
+        if (driveline !== "все") machinesData_int = machinesData_int
+            .filter(item => item['driveline_model_name'] === driveline)
+
+        if (steeringAxel !== "все") machinesData_int = machinesData_int
+            .filter(item => item['steering_axel_model_name'] === steeringAxel)
+
+        return machinesData_int
+    }, [machinesData, serial, engine, transmission, driveline, steeringAxel])
+
+    const serialUniq = useMemo(() => {
+        return [].concat({ label: "все", value: "все" }, sortBy(uniqBy(machinesData
+            .map(item => ({ 'value': item['machine_model_name'], 'label': item['machine_model_name'] }))
+            , 'label'), 'label'))
     }, [machinesData])
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
+    const engineUniq = useMemo(() => {
+        return [].concat({ label: "все", value: "все" }, sortBy(uniqBy(machinesData
+            .map(item => ({ 'value': item['engine_model_name'], 'label': item['engine_model_name'] }))
+            , 'label'), 'label'))
+    }, [machinesData])
+
+    const transmissionUniq = useMemo(() => {
+        return [].concat({ label: "все", value: "все" }, sortBy(uniqBy(machinesData
+            .map(item => ({ 'value': item['transmisson_model_name'], 'label': item['transmission_model_name'] }))
+            , 'label'), 'label'))
+    }, [machinesData])
+
+    const drivelineUniq = useMemo(() => {
+        return [].concat({ label: "все", value: "все" }, sortBy(uniqBy(machinesData
+            .map(item => ({ 'value': item['driveline_model_name'], 'label': item['driveline_model_name'] }))
+            , 'label'), 'label'))
+    }, [machinesData])
+
+    const steeringAxelUniq = useMemo(() => {
+        return [].concat({ label: "все", value: "все" }, sortBy(uniqBy(machinesData
+            .map(item => ({ 'value': item['steering_axel_model_name'], 'label': item['steering_axel_model_name'] }))
+            , 'label'), 'label'))
+    }, [machinesData])
+
+    const handleChange = (param) => (event) => {
+        switch (param) {
+            case "serial":
+                setSerial(event.target.value);
+                // console.log('handle event', param, event.target.value)
+                break;
+            case "engine":
+                setEngine(event.target.value);
+                // console.log('handle event', param)
+                break;
+            case "transmission":
+                setTransmission(event.target.value);
+                console.log('handle event', param)
+                break;
+            case "driveline":
+                setDriveline(event.target.value);
+                console.log('handle event', param)
+                break;
+            case "steeringAxel":
+                setSteeringAxel(event.target.value);
+                console.log('handle event', param)
+                break;
+
+        }
+
+
     };
 
     console.log("whoami from main", whoAmIData)
-    console.log("filteredMachineData from main", filteredMachinesData)
+    console.log("sortedMachineData from main", sortedMachinesData.map(item => ({ 'value': item.pk, 'label': item['machine_model_name'] })))
+    console.log("filteredMachineData from main 2", filteredMachinesData)
+    //const machinesDataNameUniq = sortBy(uniqBy(sortedMachinesData.map(item => ({ 'value': item.pk, 'label': item['machine_model_name'] })), 'label'), 'label')
 
-    // const store = { token: false };
-    //const accessToken = useSelector(selectAuthAccessToken);
-    // const navigate = useNavigate()
     return (
         //     {page === "main" &&
         //     <MainPage />}
@@ -59,7 +138,7 @@ function MainPage() {
                 {whoAmIData?.groups == "Менеджер" && <Text fontSize="1.5rem" fontWeight="bold" align="center" > Менеджер: {whoAmIData?.first_name} </Text>}
             </Box>
             <Center>
-                <HStack >
+                <HStack justifyContent="center">
                     <Button colorScheme="silant-b" onClick={() => setPage("swagger")}>Swagger</Button>
                     <Button colorScheme="silant-b" onClick={() => setPage("main")}>Главная страница</Button>
                     {errorAuth == undefined && <>
@@ -72,15 +151,16 @@ function MainPage() {
             </Center>
 
             <Center>
-                <HStack m="20px">
-                    <Input />
-                    <Select placeholder='Модель техники'>
-                        <option value='option1'>ПД5,0</option>
-                        <option value='option2'>ПД3,0</option>
-                        <option value='option3'>Option 3</option>
-                    </Select>
-                    <SelectSil onChange={handleChange} label="Модель техники" options={[{ value: "ПД5,0", label: "ПД5,0" }, { value: "ПД3,0", label: "ПД3,0" }]} />
-                </HStack>
+                <FormControl>
+                    <HStack m="20px" justifyContent="center">
+
+                        <SelectSil value={serial} onChange={handleChange("serial")} label="Модель техники" options={serialUniq} />
+                        <SelectSil value={engine} onChange={handleChange("engine")} label="Модель двигателя" options={engineUniq} />
+                        <SelectSil value={transmission} onChange={handleChange("transmission")} label="Модель трансмиссии" options={transmissionUniq} />
+                        <SelectSil value={driveline} onChange={handleChange("driveline")} label="Модель ведущего моста" options={drivelineUniq} />
+                        <SelectSil value={steeringAxel} onChange={handleChange("steeringAxel")} label="Модель управляемого моста" options={steeringAxelUniq} />
+                    </HStack>
+                </FormControl>
             </Center>
 
             {/* {page === "main" &&
@@ -109,7 +189,7 @@ const SelectSil = ({ label, value, options, onChange, placeholder }) => {
         <Flex alignItems="center" direction="column" justifyContent="center" >
             <FormLabel color="silant-b.300" fontWeight="600" htmlFor={id}>{label}</FormLabel >
 
-            <Select borderColor="silant-b.700" placeholder={placeholder} width="9rem" id={id} value={value} onChange={onChange}>
+            <Select borderColor="silant-b.700" placeholder={placeholder} id={id} value={value} onChange={onChange}>
                 {options.map((option) => (
                     <option value={option.value}>{option.label}</option>
                 ))}
